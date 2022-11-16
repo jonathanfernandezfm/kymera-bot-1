82367436523e7a6c5ed93637e60e257728c575e5
Jonathan Fernandez Mertanen <33097447+jonathanfernandezfm@users.noreply.github.com>
2022-11-16 14:16:44 +0000

    cambios bk

--- interactions/formulario_denegado.js
+++ interactions/formulario_denegado.js
@@ -7,7 +7,7 @@ module.exports = {
 		const previous_embed = interaction.message.embeds[0];
 		const embed_formulario = EmbedBuilder.from(previous_embed)
 			.setColor('#FF4D4D')
-			.setDescription(`❌ Denegado por el <@&1031119833024167972> - <@${interaction.user.id}>\nㅤ⠀⠀⠀⠀⠀⠀⠀\n`);
+			.setDescription(`❌ Denegado por el <@&1016042908257894460> - <@${interaction.user.id}>\nㅤ⠀⠀⠀⠀⠀⠀⠀\n`);
 
 		await interaction.message.edit({
 			embeds: [embed_formulario],
@@ -19,7 +19,7 @@ module.exports = {
 		const embed_response = new EmbedBuilder()
 			.setColor('#fc0303')
 			.setTitle('`❌` Tu formulario ha sido denegado ')
-			.setDescription('Inténtalo en otro momento')
+			.setDescription('No cumples con los requisitios, inténtalo en otro momento')
 			.setImage('https://i.imgur.com/CYz5IsA.png');
 
 		dmChannel.send({
