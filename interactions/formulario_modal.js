7416bea18b5948679a84f9efc207994877cbd7a4
BkNcs <116917089+BkNcs@users.noreply.github.com>
2022-12-23 13:22:15 +0000

    Cambios

--- interactions/formulario_modal.js
+++ interactions/formulario_modal.js
@@ -1,6 +1,7 @@
 const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
 const { canal_formulario } = require('../data/db.json');
 
+
 module.exports = {
 	name: 'formulario',
 	description: 'Formulario modal',
--- interactions/formulario_modal_submit.js
+++ interactions/formulario_modal_submit.js
@@ -1,13 +1,14 @@
 const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const { canal_formulario } = require('../data/db.json');
+const { request } = require('undici');
 
 module.exports = {
 	name: 'formulario_modal',
 	description: 'Formulario modal submit',
 	execute: async (interaction) => {
 		const nombre = interaction.fields.getTextInputValue('pregunta_nombre');
-		const horario =  
-interaction.fields.getTextInputValue('pregunta_horario');
+		const horario =
+			interaction.fields.getTextInputValue('pregunta_horario');
 		const contenido = interaction.fields.getTextInputValue('pregunta_contenido');
 		const guild = interaction.fields.getTextInputValue('pregunta_guild');
 		const rol = interaction.fields.getTextInputValue('pregunta_rol');
@@ -27,7 +28,7 @@ interaction.fields.getTextInputValue('pregunta_horario');
 			});
 		}
 
-    const playerId = playerBody.players[0].Id;
+		const playerId = playerBody.players[0].Id;
 		const playerDataResult = await request(`https://gameinfo.albiononline.com/api/gameinfo/players/${playerId}`);
 		const playerDataBody = await playerDataResult.body.json();
 		const totalFame =
@@ -47,20 +48,24 @@ interaction.fields.getTextInputValue('pregunta_horario');
 				{ name: '3) ¿Qué contenido quieres hacer en Kymera?:', value: `> ${contenido}` },
 				{ name: '4) ¿Cuál es tu anterior gremio?:', value: `> ${guild}` },
 				{ name: '5) Rol principal, secundario y armas usadas/spec:', value: `> ${rol}` },
-{ name: 'Fama estimada PvP + PvE', value: `> ${(totalFame / 10 ** 6).toFixed(2)} M` }
+				{ name: 'Fama estimada PvP + PvE', value: `> ${(totalFame / 10 ** 6).toFixed(2)} M` }
 			);
 
-		const channel = await interaction.guild.channels.fetch(canal_formulario);
+		const [person, channel] = await Promise.all([
+			interaction.guild.members.fetch(interaction.user.id),
+			interaction.guild.channels.fetch(canal_formulario),
+		]);
 
-		await channel.send({
-			content: `> <@&1016042908257894460>, nuevo formulario de <@${interaction.user.id}>`,
-			embeds: [embed],
-			components: [row],
-		});
-
-		return interaction.reply({
-			content: '`✅` Tu formulario ha sido enviado correctamente a nuestro equipo de reclutadores',
-			ephemeral: true,
-		});
+		return await Promise.all([
+			channel.send({
+				content: `> <@&1016042908257894460>, nuevo formulario de <@${interaction.user.id}>`,
+				embeds: [embed],
+				components: [row],
+			}),
+			interaction.reply({
+				content: '`✅` Tu formulario ha sido enviado correctamente a nuestro equipo de reclutadores',
+				ephemeral: true,
+			}),
+		])
 	},
 };
