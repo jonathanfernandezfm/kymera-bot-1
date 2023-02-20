d5a377f58e05a4f1cda8fe8da6fbcb582272038f
BkNcs <116917089+BkNcs@users.noreply.github.com>
2023-02-20 12:59:07 +0000

    Cambios

--- interactions/tank.js
+++ interactions/tank.js
@@ -6,7 +6,7 @@ module.exports = {
 	execute: async (interaction) => {
 		let rol = "1020271245310308394";
 		interaction.member.roles.add(rol);
-		return interaction.reply({ content: `te añadi el rol ${rol} (texto de ejemplo)`, ephemeral: true })
+		return interaction.reply({ content: `Se te ha añadido el rol <@&1076650326511337522> satisfactoriamente`, ephemeral: true })
   }
   
 };
