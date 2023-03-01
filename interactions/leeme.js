909364537831d9212d3aedf5429e11f16af1274e
BkNcs <116917089+BkNcs@users.noreply.github.com>
2023-03-01 17:03:32 +0000

    Cambios

--- interactions/leeme.js
+++ interactions/leeme.js
@@ -6,7 +6,7 @@ module.exports = {
 	execute: async (interaction) => {
 		let rol = "1080474384327782421";
 		interaction.member.roles.add(rol);
-		return interaction.reply({ content: `Te has registrado correctamente en el servidor`, ephemeral: true })
+		return interaction.reply({ content: `✅‎ <@${interaction.user.id}> Te has registrado correctamente en el servidor`, ephemeral: true })
   }
   
 };
